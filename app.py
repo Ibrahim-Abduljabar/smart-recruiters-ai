974ab46247b2601efac931bd9e8fddf3818119f9
ابراهيم عبد الجبار خان احمد خان عيسى <himo4007@gmail.com>
2026-07-04 21:38:34 +0300

    app.py

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
 import streamlit as st
+import streamlit.components.v1 as components
 from groq import Groq
 import pypdf
 import time
 
-st.markdown("""
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-NZG4MPCRBH"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-NZG4MPCRBH');
-    </script>
-""", unsafe_allow_html=True)
+with open("google_analytics.html", "r") as f:
+    components.html(f.read(), height=0)
 
 st.set_page_config(
     page_title="SmartRecruiters AI",
