aba6c70c0212796b1a31850bce348dc81a68e484
ابراهيم عبد الجبار خان احمد خان عيسى <himo4007@gmail.com>
2026-08-22 22:28:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ for index, job in enumerate(st.session_state.jobs_list):
                         """
 
                         completion = client.chat.completions.create(
-                            model="llama-3.3-70b-versatile",
+                            model="liama3_70b_8193",
                             messages=[{"role": "user", "content": prompt}]
                         )
 
